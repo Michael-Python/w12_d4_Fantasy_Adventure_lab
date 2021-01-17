89d36af3528f18f4c57e9ea698a930a239bb496c
Michael-75 <52245580+Michael-75@users.noreply.github.com>
2021-01-17 18:38:48 +0000

    Rooms getting populated.
    Still need to find way to pick a room on entry.

--- src/test/java/roomsTest/RoomTest.java
+++ src/test/java/roomsTest/RoomTest.java
@@ -10,22 +10,25 @@ import rooms.Room;
 import static org.junit.Assert.assertEquals;
 
 public class RoomTest {
-    Room one;
-    Room two;
-    Room three;
-    Room four;
-    
-    @Before 
-    public void before(){
-        one = new Room(1);
-        two = new Room(2);
-        three = new Room(3);
-        four = new Room(4);
-    }
-    
-    @Test
 
-    public void getRoomNumber(){
-        assertEquals(1, one.getRoomNumber());
+    @Before
+
+    public void before(){
+        Treasures myPrecious = new Treasures("My Precious", 1000);
+        Enemy orc = new Enemy("Orky", 50, 100);
+        Enemy ogre = new Enemy("Zsolt", 70, 120);
+        Enemy troll = new Enemy("Tony", 40, 80);
     }
+
+//        one = new Room(1);
+//        two = new Room(2);
+//        three = new Room(3);
+//        four = new Room(4);
+
+    
+//    @Test
+
+//    public void getRoomNumber(){
+//        assertEquals(1, one.getRoomNumber());
+//    }
 }
